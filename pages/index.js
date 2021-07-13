f58fb2cf75865b319ac17a7aa12aab83b47c9722
Cristiano Fernandes <cristiano.fernandes.of@gmail.com>
2021-07-13 08:12:19 -0300

    Imersão React - Aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+  margin-top: 10px;
+`
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+
+  @media(min-width: 860px) {
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea">
+        <Box>
+          <img src="https://github.com/cristianoof.png"/>
+        </Box>
+      </div>
+
+      <div className="welcomeArea">
+        <Box>
+          Bem vindo
+        </Box>
+        <Box>
+          O que deseja fazer?
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea">
+        <Box>
+          Meus Amigos
+        </Box>
+        <Box>
+          Minhas Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
